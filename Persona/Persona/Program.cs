719034ba3e62a9aac66bacb85c61c76318715cf2
AlexSanz02 <zs0128032021@unab.edu.sv>
2021-08-17 15:41:47 -0600

    LaboratorioPersona

---
+++ Persona/Persona/Program.cs
@@ -0,0 +1,24 @@
+﻿using Persona.Entidades;
+using Persona.Negocios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsPersona clspersona = new ClsPersona();
+            NClsPersona nclspersona = new NClsPersona();
+            clspersona.Nombre = "Martín";
+            clspersona.Apellido = "Tobar";
+            clspersona.Edad = 24;
+            clspersona.Sexo = "Masculino";
+            clspersona.Hablar = true;
+        }
+    }
+}
